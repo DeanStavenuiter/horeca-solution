7d4eb75635c3bd2b2a31f4ed740a401bf53f6aa1
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-08-14 21:25:05 +0200

    new home page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,68 @@
-"use client"
+"use client";
 
 import styles from "@/styles/header.module.css";
 import Link from "next/link";
 import Burger from "./Burger";
+import Image from "next/image";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { useRouter } from "next/router";
 
 const Header = () => {
+
   return (
-    <div className={styles.navbar}>
-      <div>
-        <Link href={"/"}>
-          <h1>Horeca Solution</h1>
-        </Link>
+    <header className={styles.header}>
+      <div className={styles.navbar}>
+        <div className={styles.spoed}>
+          <span>Bij spoed: +31 6 11 86 85 67</span>
+        </div>
+        <div className={styles.socials}>
+          <Link href={''} target="blank">
+            <FacebookIcon />
+          </Link>
+          <Link href={''} target="blank">
+            <InstagramIcon />
+          </Link>
+          <Link href={''} target="blank">
+            <LinkedInIcon />
+          </Link>
+        </div>
+        <div className={styles.navbarInner}>
+          <div className={styles.logoOuter}>
+            <Link 
+            href={"/"}>
+              <Image
+                className={styles.logo}
+                height={250}
+                width={250}
+                alt="logo Horeca Solution"
+                src={
+                  "https://horeca-solution.s3.eu-north-1.amazonaws.com/logo/Horeca_solution_munt_logo.png"
+                }
+              ></Image>
+            </Link>
+          </div>
+          <div className={styles.menu}>
+            <ul>
+              <li>
+                <Link href={"/"}>home</Link>
+              </li>
+              <li>
+                <Link href={"/over-ons"}>over ons</Link>
+              </li>
+              <li>
+                <Link href={"/contact"}>contact</Link>
+              </li>
+              <li>
+                <Link href={"/inloggen"}>inloggen</Link>
+              </li>
+            </ul>
+          </div>
+          <Burger />
+        </div>
       </div>
-      <div className={styles.menu}>
-        <ul>
-          <li>
-            <Link href={"/"}>home</Link>
-          </li>
-          <li>
-            <Link href={"/contact"}>contact</Link>
-          </li>
-        </ul>
-        
-      </div>
-      <Burger/>
-    </div>
+    </header>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "@/styles/page.module.css";
 import { Metadata } from "next";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 // import cook from "../../public/cook.jpg";
 export const metadata: Metadata = {
   title: "Horeca Solution - HOME",
@@ -10,54 +11,74 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <main className={styles.main}>
-        <article className={styles.homeArticle}>
-          <h1>Horeca Solution.</h1>
-          <p>
-            Horeca Solution is een bemiddelaar/uitzendbureau voor restaurants,
-            partijen en evenementen. Onze kracht ligt in het plaatsen van horeca
-            personeel
-          </p>
-          <p>
-            Voor ons is werken in de horeca topsport en draait het om snel
-            schakelen.
-          </p>
-          <p>
-            Horeca Solution levert bij strenge selectie daarom alleen maar
-            medewerkers die de passie hebben voor het vak, flexibel zijn en
-            makkelijk (mee) schakelen!
-          </p>
-          <p>
-            Sedert april 2022, zijn we het avontuur begonnen, in bemiddelen en
-            uitzenden van horeca personeel. Dit doen we iedere dag met veel
-            passie en toewijding! Onze medewerkers zijn ervaren, flexibel en
-            springen in waar er nood aan de man is. Dit kan bijvoorbeeld voor
-            een dag. Maar ook voor een lange periode.
-          </p>
-          <p>Ook bemiddelen we in hulp-koks, basis-koks, koks en chef-koks.</p>
-          <p>
-            We vinden persoonlijke aandacht voor onze professionals belangrijk.
-            We kijken daarom niet alleen naar de vaktechnische kwalificaties van
-            hen. Maar ook naar de persoonlijkheid, voorkeuren en ambities.
-          </p>
-          <p>Horeca Solution is daarom uw betrouwbare partner!</p>
-          <p>
-            Kortom, we vormen de ruggengraat van de culinaire wereld en delen we
-            graag onze vreugde, voor de kunst van het koken met u.
-          </p>
-          <p>Tot snel,</p>
-          <p>Horeca Solution.</p>
-        </article>
-        <div className={styles.imageMainOuter}>
-          <Image
-            src="https://horeca-solution.s3.eu-north-1.amazonaws.com/e2cd5063-598e-43fe-967d-48ab2452784a.JPG"
-            alt="chef"
-            className={styles.imageMain}
-          />
+        <div className={styles.mainInner}>
+          <div className={styles.HeaderImageOuter}>
+            <Image
+              className={styles.HeaderImage}
+              width={1920}
+              height={1280}
+              alt="Keuken plank met keuken spullen"
+              src={
+                "https://horeca-solution.s3.eu-north-1.amazonaws.com/fotos/restaurant-2623071_1920.jpg"
+              }
+              quality={100}
+            />
+            <div className={styles.titleOuter}>
+              <div className={styles.title}>
+                <h1>Koks, afwassers en meer.</h1>
+                <h1>Horeca Solution levert het beste personeel!</h1>
+              </div>
+            </div>
+          </div>
+          <div className={styles.middle}>
+            <div className={styles.left}>
+              <div className={styles.textLeft}>
+                <h3>Via ons aan het werk?</h3>
+                <p>Schrijf je direct in!</p>
+                <button className={styles.ctaButton}>Inschrijven!</button>
+              </div>
+            </div>
+            <div className={styles.right}>
+              <div className={styles.textRight}>
+                <h3>Vraag horeca personeel aan!</h3>
+                <p>Extra handen nodig in uw bedrijf?</p>
+                <button className={styles.ctaButton}>Ik wil personeel!</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.imageMainOuter}>
+            <video
+              className={styles.video}
+              playsInline
+              autoPlay
+              muted
+              loop
+              width={1920}
+              height={1280}
+              src="https://horeca-solution.s3.eu-north-1.amazonaws.com/fotos/fire_-_57341+(1080p).mp4"
+            ></video>
+          </div>
+          <article className={styles.homeArticle}>
+            <h1>Ons personeel is erg flexibel, snel en overal inzetbaar</h1>
+            <p>
+              Stel je voor dat je de sleutel tot naadloze samenwerking wilt
+              vinden, een partner die de brug slaat tussen jouw restaurant,
+              cateringbedrijf of evenement en het juiste personeel. Welkom bij
+              Horeca Solution, jouw bemiddelingsbedrijf in de horeca. Wij
+              begrijpen dat de culinaire wereld draait om harmonie, precisie en
+              flexibiliteit, en daarom zijn wij er om ervoor te zorgen dat de
+              juiste professionals samenkomen om meesterwerken te creëren.
+            </p>
+            <div>
+              <button className={styles.buttonOverOns}>over ons</button>
+            </div>
+          </article>
         </div>
       </main>
-    </div>
+      <Footer />
+    </>
   );
 }
